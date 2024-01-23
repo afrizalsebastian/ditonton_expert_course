f083aabfab4d42146a3ce9800f5f977709f794c5
Afrizal Sebastian <sebastiangurning@gmail.com>
2024-01-23 08:46:34 +0700

    feat: tvseries provider

--- lib/domain/usecases/tvseries/search_tvseries.dart
+++ lib/domain/usecases/tvseries/search_tvseries.dart
@@ -3,10 +3,10 @@ import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/entities/tvseries.dart';
 import 'package:ditonton/domain/repositories/tvseries_repository.dart';
 
-class SearchMovies {
+class SearchTvSeries {
   final TvSeriesRespositories repository;
 
-  SearchMovies(this.repository);
+  SearchTvSeries(this.repository);
 
   Future<Either<Failure, List<TvSeries>>> execute(String query) {
     return repository.searchTvSeries(query);
